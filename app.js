464d52f1475285924a522b0b6f856b3109643283
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-09-15 14:32:18 -0400

    initial HTML & JS set up

---
+++ app.js
@@ -0,0 +1,36 @@
+//wrap entire function in closure
+
+(function(){
+  //module takes app name and array of dependencies
+  var app = angular.module('codelink', []);
+
+  //controllers are where app's behavior is defined by functions and values
+  //pass in anon. function - code in here is what will be executed when function is called
+  app.controller('codelinkController', function(){
+    //set recommendation to product, a property of the controller
+    this.products = recommendations;
+  });
+
+  var recommendations = [
+  {
+    name: 'Code School',
+    link: 'https://www.codeschool.com/courses/shaping-up-with-angular-js',
+    description: 'I found this tutorial really helpful.',
+    languages: 'AngularJS',
+    quality: 5,
+    skillLevel: 1,
+    canReview: true,
+    doNotShow: false
+  },
+  {
+    name: 'Code Academy',
+    link: 'https://www.codecademy.com/courses/learn-angularjs',
+    description: 'It was ok, the the Code School on Angular was better.',
+    languages: 'AngularJS',
+    quality: 4,
+    skillLevel: 1,
+    canReview: true
+  }
+];
+
+})();
