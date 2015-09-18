5ff91afa608daa0045bf2e1e3f60fbadaa509c92
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-09-18 00:33:46 -0400

    updated controllers

--- js/controllers/reviews.js
+++ js/controllers/reviews.js
@@ -6,106 +6,44 @@
   var reviewControllers = angular.module('reviewControllers', ['ngRoute']);
 
   // index controller
-  reviewControllers.controller('reviewsController', ['Review', function(Review) {
-    this.reviews = Review.query();
+  reviewControllers.controller('reviewController', ['Review', function(Review) {
+    this.review = Review.all;
   }]);
 
-  // show controller (inc link to delete)
-    // reviewControllers.controller('reviewController', ['$routeParams','$location', 'Review','Comment'], function($routeParams, $location, Review){
-    //   this.grumble = Review.get({$id: $routeParams.id});
-    //   this.delete = function(id){
-    //     Review.delete({$id: id}, function(){
-    //       $location.path("/reviews");
-    //     });
-    //   };
+  // show controller (handles delete link on show page)
+  reviewControllers.controller('reviewController', ['$routeParams','$location','$http', 'Grumble','Comment', function($routeParams, $location, $http, Review, Comment){
+    this.review = Review.get({id: $routeParams.id}, function(review){
+      review.comments = Comment.query({reviewId: $routeParams.id});
+    });
+    this.delete = function(id){
+      Review.delete({id: id})
+      $location.path("/review")
+    }
+    this.createComment = function(comment){
+      var self = this
+      Comment.save({grumbleId: $routeParams.id},comment, function(comment){
+        self.review.comments.push(comment)
+        self.comment = {}
+      })
+    }
+  }]);
 
-  // new form controller
-   reviewControllers.controller('newReviewController', ["$location", 'Review', function($location, Review){
-     this.create = function(){
-       Review.save(this.review, function(review) {
-         $location.path("/reviews/" + review.$id);
-       });
-     };
-   }]);
+  // new form controller (handles creation)
+  reviewControllers.controller('newReviewController', ["$location", 'Review', function($location, Review){
+    this.create = function(){
+      var self = this
+      Review.save(this.grumble, function(id) {
+        $location.path("/review/" + id);
+      })
+    }
+  }])
 
-   // edit form controller
-   reviewControllers.controller('editGrumbleController', ["$location","$routeParams", 'Review', function($location, $routeParams, Review){
-     this.grumble = Review.get({$id: $routeParams.id});
-     this.update = function(){
-       this.review.$save();
-       $location.path("/reviews/" + this.review.$id);
-     };
-   }]);
- })();
-
-
-  // this.reviews =  [
-  //   {
-  //     "name": 'Code School',
-  //     "author": 'Everett',
-  //     "link": 'https://www.codeschool.com/courses/shaping-up-with-angular-js',
-  //     "description": 'I found this tutorial really helpful.',
-  //     "languages": 'AngularJS',
-  //     "quality": 5,
-  //     "skillLevel": 1,
-      // canReview: true,
-      // comments: [
-      //   {
-      //     author: 'Matthew',
-      //     description: 'Great tutorial',
-      //     quality: 5,
-      //     skillLevel: 2
-      //   },
-      //   {
-      //     body: 'I thought this was pretty helpful',
-      //     author: 'Sarah',
-      //     quality: 4
-      //   }
-      // ]
-    // },
-    // {
-    //   "name": 'Code Academy',
-    //   "author": 'Grace',
-    //   "link": 'https://www.codecademy.com/courses/learn-angularjs',
-    //   "description": 'It was ok, the the Code School on Angular was better.',
-    //   "languages": 'AngularJS',
-    //   "quality": 4,
-    //   "skillLevel": 1,
-      // canReview: true,
-      // comments: [
-      //   {
-      //     body: 'Not bad.',
-      //     author: 'Barb',
-      //     quality: 3
-      //   },
-      //   {
-      //     body: 'Good starting place for beginners.',
-      //     author: 'Sarah',
-      //     quality: 4
-      //   }
-      // ]
-  //   }
-  // ];
-  //
-  // this.create = function(){
-  //   this.reviews.unshift({
-  //     resourceName: this.resourceName,
-  //     link: this.link,
-  //     skillLevel: this.skillLevel,
-  //     languages: this.languages,
-  //     description: this.description,
-  //     quality: this.quality,
-  //   })
-  //   this.reset()
-  // }
-  //
-  //   this.formIsVisible = false;
-  //   this.showReview = true;
-  //   this.toggleForm = function(){
-  //     this.formIsVisible = this.formIsVisible ? false : true;
-  //   };
-  //
-  // });
-
-// })
-// ()
+  // edit form controller (handles update)
+  grumbleControllers.controller('editReviewController', ["$location","$routeParams", 'Grumble', function($location, $routeParams, Review){
+    this.review = Review.get({id: $routeParams.id})
+    this.update = function(){
+      this.review.$save();
+      $location.path("/reviews/" + this.review.$id)
+    }
+  }])
+})();
