01d06986eb1211e64e6a95c5e6300630326c2d4d
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-09-18 13:14:25 -0400

    commentFactory

---
+++ js/comment/commentFactory.js
@@ -0,0 +1,33 @@
+(function() {
+  angular
+    .module('codelink')
+    .factory("Comments", [
+      "$firebase",
+      Comments
+    ]);
+
+  function Comments($firebase){
+    var factory = this;
+    factory.for = function(grumble, comment){
+      if(!comment) comment = {}
+      var methods = {
+        $create: create,
+        $destroy: destroy
+      }
+      return angular.extend(comment, methods);
+
+      function create(){
+        if(!grumble.comments) grumble.comments = [];
+        grumble.comments.push(this);
+        return $firebase.update(grumble);
+      }
+      function destroy(){
+        grumble.comments.splice(grumble.comments.indexOf(this), 1);
+        return $firebase.update(grumble);
+        // grumble.child("comments").$remove(this)
+      }
+    }
+    return factory
+  }
+
+})();
